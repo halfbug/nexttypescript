63250defca053a6e81bcb9b1d9ab1bade7afc1ed
Sadaf <sadaf_cu@hotmail.com>
2021-10-19 11:33:55 +0500

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'no-use-before-define': 'warn',
+    'linebreak-style': 0,
+    // 'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    // 'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'react/prop-types': 'warn',
+    'import/prefer-default-export': 'warn',
+    'react/forbid-prop-types': 'warn',
+    'react/jsx-props-no-spreading': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'eol-last': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', '/'],
+      },
+    },
+  },
+};
