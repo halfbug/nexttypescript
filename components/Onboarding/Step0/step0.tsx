f85bfd4d16a5faede235089d9417c33c222a946c
Sadaf <sadaf_cu@hotmail.com>
2021-11-03 18:14:50 +0500

    react bootstrap added

--- components/Onboarding/Step0/step0.tsx
+++ components/Onboarding/Step0/step0.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Dialogue from 'components/Layout/Dialogue/dialogue';
 // import Button from '../../Buttons/Button/Button';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import styles from 'styles/Step0.module.scss';
 import HeadLogo from 'assets/images/Logo.svg';
 import Cart from 'assets/images/cart.png';
@@ -17,25 +17,38 @@ const Step0 = ({ show }:IStep0Props) => {
   console.log('welcome component');
   return (
     <Dialogue show={show}>
-      <div className={styles.WelcomeModal}>
-        <Container className={styles.headText} style={{ marginTop: '2.5vw' }}>
-          <span>Welcome to</span>
-          <img src={HeadLogo.src} alt="" />
-        </Container>
-        <Container className={styles.recommendationText} style={{ marginTop: '2.5vw' }}>
-          <span>We know that nothing beats a friend’s recommendation.</span>
-          <span style={{ marginTop: '1.5vw' }}>
-            Groupshop makes shopping together a breeze with rewards that get your
-            customers excited.
-          </span>
-        </Container>
-        <Container className={styles.shoppingCartText}>
-          <div className={styles.shoppingCartCon} style={{ marginTop: '1vw' }}>
-            <span className={styles.emoji}>
-              <img src={Cart.src} alt="cart" />
-            </span>
+      {/* <div className={styles.WelcomeModal}> */}
+      <Container className={styles.welcome}>
+        <Row className="my-4">
+          <Col>
+            <h3 className="font-weight-bold">Welcome to</h3>
+            <img src={HeadLogo.src} className={styles.welcome__img} alt="" />
+          </Col>
+        </Row>
+        <Row className="my-3">
 
-            <span>
+          <Col>
+            <h4>We know that nothing beats a friend’s recommendation.</h4>
+          </Col>
+
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={2}> </Col>
+          <Col xs={8}>
+            <h4>
+              Groupshop makes shopping together
+              a breeze with rewards that get your customers excited.
+            </h4>
+          </Col>
+          <Col xs={2}> </Col>
+        </Row>
+        <Row className="mt-3 mx-5">
+          <Col xs={1}> </Col>
+          <Col xs={2} className="mx-2">
+            <img src={Cart.src} alt="cart" />
+          </Col>
+          <Col xs={7}>
+            <p className="text-start">
               When your customers
               {' '}
               <strong>shop</strong>
@@ -47,35 +60,52 @@ const Step0 = ({ show }:IStep0Props) => {
               {' '}
               with
               friends.
-            </span>
-          </div>
-          <div className={styles.shoppingCartCon} style={{ marginTop: '1vw' }}>
-            <span className={styles.emoji}>
-
-              <img src={Face.src} alt="icon" />
-            </span>
-            <span>
+            </p>
+          </Col>
+          <Col xs={2}> </Col>
+        </Row>
+        <Row className="mt-3 mx-5">
+          <Col xs={1}> </Col>
+          <Col xs={2} className="mx-2">
+            <img src={Face.src} alt="icon" />
+          </Col>
+          <Col xs={7}>
+            <p className="text-start">
               Friends get a special discount and your customers
               <strong> earn </strong>
               real cashback every time someone shops with
               them.
-            </span>
-          </div>
-        </Container>
-        <Container className={styles.bottomText} style={{ marginTop: '1vw' }}>
-          <div className={styles.bottomTextCon}>
-            <span>FEWER PAID ADS,</span>
-            <div className={styles.paidAdsDiv} />
-          </div>
-          <div className={styles.bottomTextCon}>
-            <span>MORE ORGANIC LEADS</span>
-            <div className={styles.organicLeadsDiv} />
-          </div>
-          <div className={styles.btnCon}>
+            </p>
+          </Col>
+          <Col xs={2}> </Col>
+        </Row>
+        <Row className="m-2">
+          <Col xs={3} md={4}> </Col>
+          <Col xs={6} md={4} className="mx-2">
+            <p className="fw-bold m-0 fs-5">FEWER PAID ADS,</p>
+            <hr className="brand--gradiant mx-4" />
+          </Col>
+          <Col xs={3} md={4}> </Col>
+        </Row>
+        <Row>
+          <Col xs={3} md={4}> </Col>
+          <Col xs={6} md={4} className="mx-2">
+            <p className="fw-bold m-0 fs-5">
+              MORE ORGANIC LEADS
+            </p>
+            <hr className="brand--gradiant mx-2" />
+          </Col>
+          <Col xs={3} md={4}>&nbsp; </Col>
+        </Row>
+        <Row>
+          <Col xs={3} md={4}> </Col>
+          <Col xs={6} md={4} className="mx-2">
             <button>GET STARTED</button>
-          </div>
-        </Container>
-      </div>
+          </Col>
+          <Col xs={3} md={4}>&nbsp; </Col>
+        </Row>
+      </Container>
+      {/* </div> */}
     </Dialogue>
   );
 };
