42772266e3d4a5355aade9192038baca7295fc6b
faizaGS <92626888+faizaGS@users.noreply.github.com>
2021-12-20 22:52:16 +0500

    step 5 (#55)
    Co-authored-by: fazzyA <faz.pak@gmail.com>

--- components/Onboarding/Step5/step5.tsx
+++ components/Onboarding/Step5/step5.tsx
@@ -9,8 +9,10 @@ import { useMutation } from '@apollo/client';
 import { UPDATE_STORE } from 'store/store.graphql';
 import { IStore } from 'types/store';
 import Logo from 'assets/images/Logo.svg';
+import ConeEmoji from 'assets/images/Cone.svg';
 import { StoreContext } from 'store/store.context';
 import Router from 'next/router';
+import BrandlogoBlack from 'assets/images/brandmark1.svg';
 import ProgressBar from '../ProgressBar/ProgressBar';
 
 interface IStep5Props {
@@ -49,10 +51,22 @@ const Step5 = () => {
         <ProgressBar progress="100" />
         <Row className="my-4 ">
           <Col md={6} className="border-end pt-lg-5 mt-lg-5">
-            <Row><Col><h5>Congrats on creating your first</h5></Col></Row>
-            <Row><Col><Logo /></Col></Row>
+            <Row>
+              <Col md={3}>
+                {' '}
+                <BrandlogoBlack className="mt-3" />
+              </Col>
+              <Col className="m-4" md={7}>
+                <h5 className="mt-3">
+                  <ConeEmoji />
+                  {' '}
+                  Congrats on creating your first
+                </h5>
+                <Logo className="mt-2 m-3" />
+              </Col>
+            </Row>
             <div className="w-75 mb-lg-3">
-              <Row className="mt-4"><Col><h6>You’re on the free plan</h6></Col></Row>
+              <Row className="mt-4"><Col><h6 className="fw-bold fs-4">You’re on the free plan</h6></Col></Row>
               <Row>
                 <Col>
                   {' '}
@@ -62,28 +76,28 @@ const Step5 = () => {
                 </Col>
 
               </Row>
-              <Row className="mt-4"><Col><h6>NEXT STEPS</h6></Col></Row>
+              <Row className="mt-4"><Col><h6 className="fw-bold fs-4">Next Steps</h6></Col></Row>
               <Row>
                 <Col>
                   <ul>
-                    <li>
+                    <li className="fs-6">
                       Head to the
                       {' '}
-                      <span className="fw-bold">Overview</span>
+                      <span className="fw-bold fs-5">Overview</span>
                       {' '}
                       dashboard to track your campaign’s progress
                     </li>
-                    <li>
+                    <li className="fs-6">
                       Check your
                       {' '}
-                      <span className="fw-bold">Analytics</span>
+                      <span className="fw-bold fs-5">Analytics</span>
                       {' '}
                       for advanced first-party data
                     </li>
-                    <li>
+                    <li className="fs-6">
                       Explore more features on the
                       {' '}
-                      <span className="fw-bold">Settings</span>
+                      <span className="fw-bold fs-5">Settings</span>
                       {' '}
                       page
                     </li>
@@ -94,74 +108,53 @@ const Step5 = () => {
             <Row><Col><Button onClick={handleClick}>Lets Go</Button></Col></Row>
           </Col>
           <Col md={6} className={styles.letsgo__left}>
-            <Row className={styles.letsgo__box}>
-              <Col xs={4} className={styles.letsgo__box_col1}><h2>Free</h2></Col>
-              <Col xs={8} className={styles.letsgo__box_col2}>
+            <Row className={styles.box_row}>
+              <Col className={styles.side_box}>
                 <h2>Explore</h2>
-                <p>
-                  First 100 Groupshops
-
-                  <p className="text-muted">From us, for everyone.</p>
-                </p>
+                <Button className={styles.free}>Free</Button>
+                <p>First 100 Groupshops</p>
               </Col>
-            </Row>
-            <Row className={styles.letsgo__box}>
-              <Col xs={4} className={styles.letsgo__box_col1}>
-                <h2>25¢</h2>
-                <p className="text-muted">
-                  per Groupshops
-
-                </p>
-              </Col>
-              <Col xs={8} className={styles.letsgo__box_col2}>
+              <Col className={styles.side_box}>
                 <h2>Launch</h2>
+                <Button className={styles.btn}>25¢ per Groupshop</Button>
                 <p>
                   Up to 1,000 Groupshops
-                  <p className="text-muted">
-                    For small, ambitious businesses.
-                  </p>
+                  {' '}
+                  <br />
+                  {' '}
+                  per month
                 </p>
-
               </Col>
             </Row>
-            <Row className={styles.letsgo__box}>
-              <Col xs={4} className={styles.letsgo__box_col1}>
-                <h2>20¢</h2>
-                <p className="text-muted">
-                  per Groupshops
-                </p>
-              </Col>
-              <Col xs={8} className={styles.letsgo__box_col2}>
+            <Row className={styles.box_row}>
+              <Col className={styles.side_box}>
                 <h2>Growth</h2>
+                <Button className={styles.btn}>20¢ per Groupshop</Button>
                 <p>
                   Up to 2,500 Groupshops
-                  <p className="text-muted">
-                    For high-growth brands.
-                  </p>
+                  {' '}
+                  <br />
+                  {' '}
+                  per month
+
                 </p>
               </Col>
-            </Row>
-            <Row className={styles.letsgo__box}>
-              <Col xs={4} className={styles.letsgo__box_col1}>
-                <h2>10¢</h2>
-                <p className="text-muted">
-                  per Groupshop
-                </p>
-              </Col>
-              <Col xs={8} className={styles.letsgo__box_col2}>
-                <h2> Enterprise </h2>
+              <Col className={styles.side_box}>
+                <h2>Unicorn</h2>
+                <Button className={styles.btn}>10¢ per Groupshop</Button>
                 <p>
                   2,500+ Groupshops
-                  <p className="text-muted">
-                    For leading companies.
-                  </p>
+                  {' '}
+                  <br />
+                  {' '}
+                  per month
+
                 </p>
               </Col>
             </Row>
           </Col>
         </Row>
       </Container>
-      {/* </div> */}
     </Dialogue>
   );
 };
