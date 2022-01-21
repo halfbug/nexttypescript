393b71cfb9f2d8cdacc26c2cee2e19c51513c592
Sadaf Siddiqui <92418104+sadafgs@users.noreply.github.com>
2022-01-21 21:21:05 +0500

    add refetch on overview (#92)
    Co-authored-by: Sadaf <sadaf_cu@hotmail.com>

--- pages/[shop]/overview.tsx
+++ pages/[shop]/overview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import type { NextPage } from 'next';
 // import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -7,12 +7,15 @@ import useInstallation from 'hooks/useInstallation';
 import { StoreContext } from 'store/store.context';
 import { useQuery } from '@apollo/client';
 import { GET_STORE } from 'store/store.graphql';
+import { IStore } from 'types/store';
 // import useStore from 'hooks/useStore';
 
 const ShopMain: NextPage = () => {
   const { query: { shop, ins } } = useRouter();
 
-  const { loading, error, data } = useQuery(GET_STORE, {
+  const {
+    loading, error, data, refetch,
+  } = useQuery(GET_STORE, {
 
     variables: { shop },
   });
@@ -20,29 +23,38 @@ const ShopMain: NextPage = () => {
   const { installationDialogue } = useInstallation(ins);
 
   const { store, dispatch } = useContext(StoreContext);
-  // console.log('🚀 ~ file: [ins].tsx ~ line 21 ~ data', data);
+  const [jsonobj, setjsonobj] = useState<any | undefined>(undefined);
+  console.log('🚀 ~ file: [ins].tsx ~ line 21 ~ data', data);
   // console.log('🚀 ~ file: [ins].tsx ~ line 21 ~ error', error);
   // console.log('🚀 ~ file: [ins].tsx ~ line 21 ~ loading', loading);
 
   useEffect(() => {
     if (data) {
       dispatch({ type: 'UPDATE_STORE', payload: data?.storeName });
+      if (!jsonobj && !loading && data) {
+        const obj = { ...store };
+        delete obj.products;
+        delete obj.collections;
+        setjsonobj(obj);
+      }
     }
     // return () => {
     //   cleanup
     // }
   }, [data]);
 
-  const obj = { ...store };
-  delete obj.products;
-  delete obj.collections;
-  const jsonStore = JSON.stringify(obj, null, '\t');
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const jsonStore = JSON.stringify(jsonobj, null, '\t');
   return (
     <Page headingText="Overview" onLogin={() => {}} onLogout={() => {}} onCreateAccount={() => {}}>
       dashboard overview
       {shop}
-      <div><pre>{ jsonStore }</pre></div>
-
+      {/* <div><pre>{ jsonStore }</pre></div> */}
+      {loading && <p>.......loading....</p>}
+      {data && <p>{JSON.stringify(data, null, '\t')}</p>}
       {/* {installationDialogue()} */}
 
     </Page>
