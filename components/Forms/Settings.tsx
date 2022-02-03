a5b4e62af25725b5def9a625868c2c8f30733305
Komal Ata <komal.ata005@gmail.com>
2022-02-03 11:58:55 +0500

    new db setting custom theme set according to figma (#103)
    * new db setting custom theme set according to figma
    
    * add flaxibility in theme toggle button
    
    Co-authored-by: Sadaf <sadaf_cu@hotmail.com>

--- components/Forms/Settings.tsx
+++ components/Forms/Settings.tsx
@@ -158,7 +158,7 @@ export default function Settings({ isDB }: IProps) {
         <Form.Text className="text-muted mt-0 fs-6">You’ll want a color that stands out on a white background</Form.Text>
       </Row>
       <Row className="mt-3">
-        <Col xs={12}>
+        <Col lg={12}>
           <Form.Group className="d-flex ">
             <span className={styles.ob_settings__brandcolor}>
               <Form.Label htmlFor="brandColor" className="m-0 py-1 px-3 pe-5">Click to pick</Form.Label>
@@ -192,37 +192,43 @@ export default function Settings({ isDB }: IProps) {
           <Form.Text className="text-muted mt-0 fs-6">This will serve as the banner for your Groupshop page </Form.Text>
         </Col>
       </Row>
-      <Row className="border rounded p-4">
-        <Col xs={6} className="w-75">
-          <h6>Pre-Set Themes</h6>
-          <Row>
-            <ButtonGroup className={['mb-2', styles.ob_settings__group].join(' ')}>
-              {radios.map(({ name, component, value }) => (
-                <ToggleButton
-                  key={name}
-                  id={`radio-${name}`}
-                  type="radio"
-                  variant="none"
-                  name="radio"
-                  value={value}
-                  checked={values.customBg === value}
-                  onChange={(e) => {
-                    setFieldValue('customBg', e.currentTarget.value);
-                    handleForm();
-                  }}
-                  bsPrefix={styles.ob_settings_hide}
-                  className={styles.ob_settings__radio}
-                >
-                  {component}
-                </ToggleButton>
-              ))}
-            </ButtonGroup>
-          </Row>
+      <Row className=" border rounded px-1 py-3 my-1 mt-2">
+        <Col md={6}>
+          <h6 className="fs-6 fw-bolder lh-base">Pre-Set Themes</h6>
+          {/* <Row> */}
+          <ButtonGroup className={["mb-2 d-block mx-0 p-0", styles.ob_settings__group].join(' ')}>
+            {radios.map(({ name, component, value }) => (
+              <ToggleButton
+                key={name}
+                id={`radio-${name}`}
+                type="radio"
+                variant="none"
+                name="radio"
+                value={value}
+                checked={values.customBg === value}
+                onChange={(e) => {
+                  setFieldValue('customBg', e.currentTarget.value);
+                  handleForm();
+                }}
+                bsPrefix={styles.ob_settings_hide}
+                className={styles.ob_settings__radio}
+              >
+                {component}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+          {/* </Row> */}
         </Col>
-        <Col xs={6}>
-          <Row className="mt-2"><h6>Custom background</h6></Row>
+        <Col md={6} className={styles.vertical}>
+          <Row className="mt-2">
+            <h6 className="fs-6 fw-bolder lh-base text-nowrap">
+              Custom background
+              <span className={styles.badge}>Recommended</span>
+            </h6>
+
+          </Row>
           <Row>
-            <Col xs={12} className="align-middle mt-2">
+            <Col lg={12} className="align-middle mt-2">
               <Form.Check
                 inline
                 label="Image"
@@ -273,15 +279,13 @@ export default function Settings({ isDB }: IProps) {
               />
               <p className="text-muted">Please paste youtube video URL</p>
             </Col>
+            <Row>
+              <Col className="text-muted align-right text-end">
+                <h6 className={styles.smallt}> Under 5mb (PNG, JPG, JPEG)</h6>
+                <h6 className={styles.smallt}> 1440px x 500px</h6>
+              </Col>
+            </Row>
           </Row>
-          <Row>
-            <Col className="text-muted align-right text-end">
-              Under 5mb (PNG, JPG, JPEG)
-              <br />
-              1440px x 500px
-            </Col>
-          </Row>
-
         </Col>
       </Row>
       <Row />
