6b7d0faf7baec2fe694b7b691ac1d986ffafa1c7
Komal Ata <komal.ata005@gmail.com>
2022-02-17 11:54:10 +0500

    Set footer (#148)
    * img and x margin set
    
    * footer link
    
    * subscribe button
    
    * counter
    
    * .
    
    * fixed horizontal scroll
    
    * ..
    
    * counter
    
    * all done
    
    * logo margin
    
    * imges name change to lowercase and deleted extra files
    
    * fix
    
    Co-authored-by: Sadaf <sadaf_cu@hotmail.com>

--- components/Layout/FooterGS/FooterGS.tsx
+++ components/Layout/FooterGS/FooterGS.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import footerLogo1 from 'public/images/groupshopLogo.svg';
+import footerLogo from 'public/images/groupshoplogo.png';
+import footerLogo1 from 'public/images/logo-thin.png';
 import SocialButton from 'components/Buttons/SocialButton/SocialButton';
 import styles from 'styles/Groupshop.module.scss';
 
 import {
-  Col, Row, Button, FormControl, InputGroup,
+  Col, Row, Button, FormControl, InputGroup, Container,
 } from 'react-bootstrap';
 import { ChevronRight } from 'react-bootstrap-icons';
 
@@ -18,109 +19,100 @@ const Footer = ({
   LeftComp, RightComp,
 }: FooterProps) => (
 
-  <>
+  <Container fluid className={styles.groupshop_footer}>
     <hr />
-    <Row className=" w-100 align-items-center">
-      <Col xs={12}>
-        {' '}
-        <div className="d-flex justify-content-around">
-          <Col sm={3} className="p-2 justify-content-start">
-            <p className=""><b>Complete your order in time to benefit from these exclusive rewards!</b></p>
-            <div className={styles.groupshop_counter_footer}>
-              <p className="pt-2">
-                <span>
-                  {' '}
-                  3
-                </span>
+    <Row className={styles.groupshop_footer_f1}>
+      <Col lg={3}>
+        <p className="mb-5 w-75"><b>Complete your order in time to benefit from these exclusive rewards!</b></p>
+        <Row className={styles.groupshop_footer_counter}>
+          <Col className="d-flex ">
+            <div className="text-center m-1">
+              <span>
                 {' '}
-                :
-                <span>
-                  {' '}
-                  15
-                </span>
-                {' '}
-                :
-                <span>
-                  {' '}
-                  3
-                </span>
+                3
+              </span>
+              <p className="fw-bold">DAYS</p>
+            </div>
+            <div className="py-4 mx-2">
+              {' '}
+              :
+            </div>
 
-              </p>
-              <div className="d-flex">
-                <span className="px-2 fw-bold fs-6">
-                  {' '}
-                  DAYS
-                </span>
+          </Col>
+          <Col className="d-flex mx-0 px-0">
+            <div className="text-center m-1">
+              <span>
+                15
+              </span>
+              <p className="fw-bold">HOURS</p>
+            </div>
+            <div className="py-4 mx-2">
+              {' '}
+              :
+            </div>
 
-                <span className="px-3 fw-bold fs-6">
-                  {' '}
-                  HRS
-                </span>
-
-                <span className="px-3 fw-bold fs-6">
-                  {' '}
-                  MINS
-                </span>
-
-              </div>
+          </Col>
+          <Col className="d-flex mx-0 px-0">
+            <div className="text-center m-1">
+              <span>
+                3
+              </span>
+              <p className="fw-bold">MINUTES</p>
             </div>
           </Col>
-          <Col sm={3} className="p-0 justify-content-center ">
-            <Row className="pt-2 d-flex justify-content-center">
-              {' '}
-              <img src={footerLogo1} width={166.26} height="12.26" className="justify-content-center" alt="brandLogo" />
-            </Row>
-            <Row className="mx-0, px-0 pt-3">
-              <Col className="d-flex justify-content-end"><SocialButton network="Instagram" url={'  '} /></Col>
-              <Col className="d-flex justify-content-end">
+
+        </Row>
+      </Col>
+      <Col lg={3}>
+        <Row className={['mx-5 my-4', styles.groupshop_footer__logo].join(' ')}>
+          <img src={footerLogo.src} alt="brandLogo" />
+        </Row>
+        <Row className={styles.groupshop_socialIcon}>
+          <Row className="mx-5 w-75 px-0">
+            <Row className="mx-2">
+              <Col><SocialButton network="Instagram" url={'  '} /></Col>
+              <Col>
                 <SocialButton network="Youtube" url={' '} />
               </Col>
-              <Col className="d-flex justify-content-end"><SocialButton network="Tiktok" url={'  '} /></Col>
-              <Col className="d-flex justify-content-end"><SocialButton network="Twitter" url={'  '} /></Col>
+              <Col><SocialButton network="Tiktok" url={'  '} /></Col>
+              <Col><SocialButton network="Twitter" url={'  '} /></Col>
             </Row>
-            <Row className="mx-0, px-0 pt-3">
-              <Col className="d-flex justify-content-end fw-bold ">About</Col>
-              <Col className="d-flex justify-content-start fw-bold ">FAQ</Col>
-            </Row>
-          </Col>
-          <Col sm={3} className="p-2 justify-content-end">
-            <p className="">
-              {' '}
-              <b>Want your own store?</b>
-              Be the first to find out when you can shop your favorite brands on Groupshop.
-
-            </p>
-            <InputGroup className=" mx-0 px-1 col-lg-6">
-              <FormControl
-                placeholder="Enter your email"
-                aria-label="Email"
-                aria-describedby="basic-addon2"
-                className="border-0 border-bottom px-2"
-              />
-              <Button size="lg" variant="outline-primary border border-primary">
-                <ChevronRight size={16} />
-              </Button>
-            </InputGroup>
-          </Col>
-        </div>
+          </Row>
+        </Row>
+        <Row className={['d-flex', styles.groupshop_footer_link].join(' ')}>
+          <div className=" text-center ">
+            <Button variant="link" className="fw-bold">About</Button>
+            <Button variant="link" className="fw-bold">FAQ</Button>
+          </div>
+        </Row>
+      </Col>
+      <Col lg={3}>
+        <p className="w-75">
+          <b>Want your own store? </b>
+          Be the first to find out when you can shop your favorite brands on Groupshop.
+        </p>
+        <InputGroup className=" my-3 w-75" id="borderclr">
+          <FormControl
+            placeholder="Enter your email"
+            aria-label="Email"
+            aria-describedby="basic-addon2"
+            className="border-0 border-bottom px-2"
+          />
+          <Button size="sm" variant="outline" className={styles.groupshop_footer_sub}>
+            <ChevronRight size={16} />
+          </Button>
+        </InputGroup>
       </Col>
     </Row>
-    {/* <hr /> */}
-    <Row className=" border-top d-flex justify-content-center">
-      <div className="mt-1 d-flex justify-content-center align-items-center ">
-        {' '}
-        <p className="mt-3 d-flex justify-content-center align-items-center mx-2">Powered by</p>
-        {' '}
-        {' '}
-        <img src={footerLogo1} alt="Logo" className="d-flex justify-content-center align-items-center mx-1" width={112} />
+    <hr />
+    <Row>
+      <div className={styles.groupshop_footer_f2}>
+        <p>Powered by</p>
+        <img src={footerLogo1.src} alt="Logo" className=" mx-1" width={112} />
       </div>
     </Row>
-  </>
+  </Container>
 
 );
-
-// Footer.defaultProps = {
-//   user: {},
-// };
 
 export default Footer;
