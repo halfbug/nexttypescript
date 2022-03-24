d7c31dcaeb50ece3456075bb6055da1c823bb9c6
Komal Ata <komal.ata005@gmail.com>
2022-03-24 20:32:58 +0500

    Revert "Knowledge base (#306)" (#314)
    This reverts commit 502a582900df3f5140bb4ce5ea58dcae0066acac.

--- pages/[shop]/knowledgebase.tsx
+++ pages/[shop]/knowledgebase.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Page from 'components/Layout/Page/Page';
-import { GsExperience } from 'components/Widgets/GsExperience';
-import { GsGuide } from 'components/Widgets/GsGuide';
-import { Col, Container, Row } from 'react-bootstrap';
-import { GsFeartured } from 'components/Widgets/GsFeartured';
-import { FAQs } from 'components/Widgets/FAQs';
-import ConnectSocialMedia from 'components/Widgets/ConnectSocialMedia';
 
 const KnowledgeBase: NextPage = () => (
-  <Page headingText="Knowledge base" onLogin={() => {}} onLogout={() => {}} onCreateAccount={() => {}}>
-    <Row className="mt-5">
-      <Col lg={7} className="p-0">
-        <GsExperience />
-        <GsGuide />
-        <GsFeartured />
-      </Col>
-      <Col lg={5}>
-        <FAQs />
-        <ConnectSocialMedia />
-      </Col>
-    </Row>
+  <Page headingText="Knowledge Base" onLogin={() => {}} onLogout={() => {}} onCreateAccount={() => {}}>
+    Knowledge Base
   </Page>
 );
 
