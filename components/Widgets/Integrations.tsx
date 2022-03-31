fb73cd80d539d24514a0d0a64f0543fbd8f6d069
Komal Ata <komal.ata005@gmail.com>
2022-03-31 22:33:07 +0500

    Integration widget (#322)
    * connect your pixels
    
    * .

--- components/Widgets/Integrations.tsx
+++ components/Widgets/Integrations.tsx
@@ -2,9 +2,13 @@
 import * as React from 'react';
 import styles from 'styles/Marketing.module.scss';
 import {
-  Row, Col,
+  Row, Col, FormControl, InputGroup,
 } from 'react-bootstrap';
 import Inte from 'assets/images/Integrations.svg';
+import Fa from 'assets/images/FacebookPixel.svg';
+import Ga from 'assets/images/GooglePixel.svg';
+import Ta from 'assets/images/TikTokPixel.svg';
+import { InfoCircle } from 'react-bootstrap-icons';
 
 export default function Integrations(
 ) {
@@ -15,7 +19,52 @@ export default function Integrations(
         <h4 className="mt-0">
           Connect your pixels
         </h4>
-        <p>Track customer data and behavior on your online store</p>
+        <p className="mt-2">Track customer data and behavior on your online store</p>
+        <div>
+          <Row>
+            <div className="d-flex">
+              <Fa className="me-2" />
+              <Col lg={6} className="style.marketing_inputIconBox">
+                <InputGroup className="mb-3 ">
+                  <FormControl
+                    placeholder="Enter Facebook Pixel"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+                {/* <InfoCircle className="styles.marketing_inputIcon" fill="0F0E0E" /> */}
+              </Col>
+            </div>
+          </Row>
+          <Row>
+            <div className="d-flex">
+              <Ta className="me-2" />
+              <Col lg={6}>
+                <InputGroup className="mb-3 ">
+                  <FormControl
+                    placeholder="Enter Tik Tok Pixel"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+              </Col>
+            </div>
+          </Row>
+          <Row>
+            <div className="d-flex">
+              <Ga className="me-2" />
+              <Col lg={6}>
+                <InputGroup className="mb-3 ">
+                  <FormControl
+                    placeholder="Enter Google Pixel"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+              </Col>
+            </div>
+          </Row>
+        </div>
       </section>
       <section className={styles.marketing__box_1}>
         <Row>
