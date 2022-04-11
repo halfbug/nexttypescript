5908b14ba70d456723f5ace5378d753329af29de
Komal Ata <komal.ata005@gmail.com>
2022-04-11 15:45:51 +0500

    image fixes stretching removes (#339)

--- components/QrStoreDetails/QrStep1.tsx
+++ components/QrStoreDetails/QrStep1.tsx
@@ -184,6 +184,7 @@ export default function QrStep1({
                     width="100%"
                     height="100%"
                     layout="responsive"
+                    className={styles.QRContainer__fit__image}
                   />
                 </div>
               </div>
