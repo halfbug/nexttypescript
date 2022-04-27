c266b7d379dc33604bbb2d41c4ffd580401b108a
Faiza Aziz <92626888+faizaGS@users.noreply.github.com>
2022-04-27 17:33:35 +0500

    hide popup on members card names (#449)

--- components/Groupshop/Members/Members.tsx
+++ components/Groupshop/Members/Members.tsx
@@ -25,29 +25,31 @@ const Members = ({
         placement="bottom"
         // delay={200}
         // flip
-        overlay={(
-          <Popover id={`popover-positioned-${member}`} arrowProps={() => {}}>
-            {/* <Popover.Header as="h3">{`Popover ${member}`}</Popover.Header> */}
-            <Popover.Body>
-              <div className={['d-flex justify-content-end mb-1 ', styles.groupshop__popover_cross].join('')}>
-                <CrossICon />
-              </div>
-              <h4>
+        overlay={(<></>
+          // <Popover id={`popover-positioned-${member}`} arrowProps={() => {}}>
+          //   {/* <Popover.Header as="h3">{`Popover ${member}`}</Popover.Header> */}
+          //   <Popover.Body>
+          //     <div
+          // className={['d-flex justify-content-end mb-1 ',
+          // styles.groupshop__popover_cross].join('')}>
+          //       <CrossICon />
+          //     </div>
+          //     <h4>
 
-                {member}
-                {index}
-              </h4>
-              <p className="mb-2">
-                {index === 0 ? '👑GROUPSHOP OWNER' : 'GROUPSHOP MEMBER'}
-                {' '}
-              </p>
-              <p>
-                {cashback[index]}
-                {' '}
-                in discounts and cashback.
-              </p>
-            </Popover.Body>
-          </Popover>
+          //       {member}
+          //       {index}
+          //     </h4>
+          //     <p className="mb-2">
+          //       {index === 0 ? '👑GROUPSHOP OWNER' : 'GROUPSHOP MEMBER'}
+          //       {' '}
+          //     </p>
+          //     <p>
+          //       {cashback[index]}
+          //       {' '}
+          //       in discounts and cashback.
+          //     </p>
+          //   </Popover.Body>
+          // </Popover>
       )}
       >
         <Button variant="light" className={`${index === 0 ? styles.groupshop__top_item_owner : styles.groupshop__top_item}`}>
