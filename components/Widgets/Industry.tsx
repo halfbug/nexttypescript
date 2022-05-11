ca525f12eeb4422df1a650b47f6cf31199142d36
Komal Ata <komal.ata005@gmail.com>
2022-05-11 21:32:52 +0500

    Industry Issue lists (#551)

--- components/Widgets/Industry.tsx
+++ components/Widgets/Industry.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from 'styles/GeneralForm.module.scss';
 import {
-  Col, Row,
+  Col, Row, Form,
 } from 'react-bootstrap';
 
 export interface IAppProps {
@@ -24,11 +24,11 @@ export function Industry(
     <section className={styles.generalform_greenbox}>
       <h4 className={styles.generalform_subheading}>Your Industry</h4>
       <Row>
-        <h6 className={styles.generalform_light_txt}>
+        <h6 className={['pt-2', styles.generalform_light_txt].join(' ')}>
           This informs calculations for your Analytics page.
         </h6>
       </Row>
-      <select
+      {/* <select
         aria-label="Default select example"
         name="industry"
         className={styles.generalform__select}
@@ -47,7 +47,26 @@ export function Industry(
         <option value="Food & Beverage">Food & Beverage</option>
         <option value="Outdoors">Outdoors</option>
         <option value="Pets">Pets</option>
-      </select>
+      </select> */}
+      <Form.Select
+        aria-label="Default select example"
+        className={['mt-2', styles.generalform__select].join(' ')}
+        onChange={(e) => {
+          handleForm('industry', e.currentTarget.value);
+        }}
+        defaultValue={values.industry}
+        value={values.industry}
+      >
+        <option>Click to select</option>
+        <option value="Clothing/Fashion">Clothing/Fashion</option>
+        <option value="Beauty & Self-Care">Beauty & Self-Care</option>
+        <option value="Accessories">Accessories</option>
+        <option value="Wellness">Wellness</option>
+        <option value="Home">Home</option>
+        <option value="Food & Beverage">Food & Beverage</option>
+        <option value="Outdoors">Outdoors</option>
+        <option value="Pets">Pets</option>
+      </Form.Select>
     </section>
 
   );
