e6c8b97912ccd96cba8f2be147e36e85ff137f5c
Komal Ata <komal.ata005@gmail.com>
2022-05-20 19:46:39 +0500

    807 (#627)
    * 807
    
    * GS-810
    
    * GS-811

--- components/Widgets/AffiliateDetail.tsx
+++ components/Widgets/AffiliateDetail.tsx
@@ -10,8 +10,8 @@ import OrderFromLogo from 'assets/images/order-from.svg';
 
 export default function AffiliateDetail() {
   return (
-    <section className={styles.partner__box_1}>
-      <div className="pb-3">
+    <section className={styles.partner__box_last}>
+      <div className="pb-3 pe-3">
         <h4 className="mt-0">
           llian Davis
           {' '}
@@ -26,39 +26,47 @@ export default function AffiliateDetail() {
             <span className={styles.partner__detail_tag__tag1}>$428 generated</span>
           </Col>
           <Col lg={6} md={6} xs={6}>
-            <span className={styles.partner__detail_tag__tag2}>
-              <UniqueClicksLogo />
+            <span className={['pe-2 ps-1', styles.partner__detail_tag__tag2].join(' ')}>
+              {/* <UniqueClicksLogo /> */}
+              👆 148 unique clicks
             </span>
           </Col>
         </Row>
         <Row className="mt-2">
           <Col lg={6} md={6} xs={6}>
             <span className={styles.partner__detail_tag__tag3}>
-              <NewPurchaseLogo />
+              {/* <NewPurchaseLogo /> */}
+              🛒  3 purchases
             </span>
           </Col>
           <Col lg={6} md={6} xs={6}>
-            <span className={styles.partner__detail_tag__tag4}>
-              <NewCustomerLogo />
+            <span className={['pe-2 ps-1', styles.partner__detail_tag__tag4].join(' ')}>
+              {/* <NewCustomerLogo /> */}
+              ✨ 2 new customers
             </span>
           </Col>
         </Row>
         <Row className="mt-2">
           <Col lg={6} md={6} xs={6}>
             <span className={styles.partner__detail_tag__tag5}>
-              <CashBackLogo />
+              {/* <CashBackLogo /> */}
+              💸 $112 cashback
             </span>
           </Col>
           <Col lg={6} md={6} xs={6} className="d-flex justify-content-start align-items-center">
-            <span className={styles.partner__btn}>View Groupshop</span>
-            <ArrowRightLogo />
+            <div className={styles.partner__btn}>
+              <span>View Groupshop</span>
+              {' '}
+              <ArrowRightLogo />
+            </div>
           </Col>
         </Row>
 
         <hr className={styles.partner__sperator} />
 
-        <Row>
-          <OrderFromLogo />
+        <Row className={styles.partner_order}>
+          {/* <OrderFromLogo /> */}
+          🔗 Orders from this Groupshop
         </Row>
         <Row className="mt-3">
           <Col lg={4} md={4} xs={4} className={styles.partner__simple_txt}>W83HFSD</Col>
