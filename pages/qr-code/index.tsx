85d349eba326152551347c2a05b079518aab8286
Sadaf Siddiqui <92418104+sadafgs@users.noreply.github.com>
2022-06-01 20:38:59 +0500

    GS-986 added clarity to qr page (#672)
    Co-authored-by: Sadaf <sadaf_cu@hotmail.com>

--- pages/qr-code/index.tsx
+++ pages/qr-code/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import type { NextPage } from 'next';
-
+import Head from 'next/head';
 // import components
 import QrStep1 from 'components/QrStoreDetails/QrStep1';
 import QrStep2 from 'components/QrStoreDetails/QrStep2';
@@ -15,6 +15,19 @@ const QrCode: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <title>Groupshop - QR code</title>
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "9j6d12pkzs");`,
+          }}
+        />
+      </Head>
       {showStep1 && (
         <QrStep1
           setShowStep1={setShowStep1}
