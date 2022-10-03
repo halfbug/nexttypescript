615357b40c1bd8dad9aef036984be35ddc0e8b58
Amit Batra <86948970+amit-groupshop@users.noreply.github.com>
2022-10-04 00:20:35 +0530

    tracking code update (#1222)

--- pages/[shop]/deal/[...code].tsx
+++ pages/[shop]/deal/[...code].tsx
@@ -291,8 +291,7 @@ const GroupShop: NextPage<{ meta: any }> = ({ meta }: { meta: any }) => {
         <script
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        console.log("🚀 ~ file: [...code].tsx ~ line 304 ~ useEffect ~ gsctx?.store?.products", gsctx?.store?.products)
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':        
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
