1919a6230a87ae48fc1de8b7c13e4bbfd2e9d98b
Amit Batra <86948970+amit-groupshop@users.noreply.github.com>
2022-10-06 16:13:32 +0530

    GS-1620 10 name tags in one row (#1240)
    10 name tags in one row

--- components/Groupshop/Members/Members.tsx
+++ components/Groupshop/Members/Members.tsx
@@ -42,45 +42,49 @@ const Members = ({
     <>
 
       {names?.map((member, idx) => (
-        <div>
-          <Button onClick={(e) => handleClick(e, `m-${idx}`)} variant="light" className={styles.groupshop__top_item}>
-            {idx === 0 && '👑'}
-            {' '}
-            {member}
-          </Button>
+        <>
+          <div>
+            <Button onClick={(e) => handleClick(e, `m-${idx}`)} variant="light" className={styles.groupshop__top_item}>
+              {idx === 0 && '👑'}
+              {' '}
+              {member}
+            </Button>
 
-          <Overlay
-            rootClose
-            show={show[`m-${idx}`] || false}
-            target={target}
-            placement={isLargeScreen ? 'bottom' : 'top'}
-            onHide={() => setShow({ ...show, [`m-${idx}`]: false })}
-
-          >
-            <Popover id={`popover-positioned-${idx}`}>
-              <Popover.Body>
-                <div
-                  className={['d-flex justify-content-end mb-1 ',
-                    styles.groupshop__popover_cross].join('')}
-                >
-                  <CrossICon onClick={(e: any) => handleClose(e, `m-${idx}`)} />
-                </div>
-                <h4>
-                  {member}
-                </h4>
-                <p className="mb-2">
-                  {idx === 0 ? '👑GROUPSHOP OWNER' : 'GROUPSHOP MEMBER'}
-                  {' '}
-                </p>
-                {/* <p>
+            <Overlay
+              rootClose
+              show={show[`m-${idx}`] || false}
+              target={target}
+              placement={isLargeScreen ? 'bottom' : 'top'}
+              onHide={() => setShow({ ...show, [`m-${idx}`]: false })}
+            >
+              <Popover id={`popover-positioned-${idx}`}>
+                <Popover.Body>
+                  <div
+                    className={['d-flex justify-content-end mb-1 ',
+                      styles.groupshop__popover_cross].join('')}
+                  >
+                    <CrossICon onClick={(e: any) => handleClose(e, `m-${idx}`)} />
+                  </div>
+                  <h4>
+                    {member}
+                  </h4>
+                  <p className="mb-2">
+                    {idx === 0 ? '👑GROUPSHOP OWNER' : 'GROUPSHOP MEMBER'}
+                    {' '}
+                  </p>
+                  {/* <p>
                   {cashback[idx]}
                   {' '}
                   in discounts and cashback.
                 </p> */}
-              </Popover.Body>
-            </Popover>
-          </Overlay>
-        </div>
+                </Popover.Body>
+              </Popover>
+            </Overlay>
+          </div>
+          {(idx + 1) % 10 === 0 && (
+          <div className={styles.groupshop__member_line_break} />
+          )}
+        </>
       ))}
     </>
   );
