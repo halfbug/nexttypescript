3c5c1ba54de110de18e9b623a6950f342fbe9545
Sadaf Siddiqui <92418104+sadafgs@users.noreply.github.com>
2022-10-28 19:11:46 +0500

    FB Domain verification deployment  and Redirection to groupshop.com/consumers (#1363)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { NextPage } from 'next';
 // import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Page from '../components/Layout/Page/Page';
 import useInstallation from '../hooks/useInstallation';
 // import useStore from 'hooks/useStore';
 
 const Home: NextPage = () => {
-  const { query: { shop, ins } } = useRouter();
+  const { query: { shop, ins }, push } = useRouter();
   const { installationDialogue } = useInstallation(ins);
   // const { merchantStore } = useStore(sci || undefined);
 
+  useEffect(() => {
+    if (!shop) { push('https://www.groupshop.com/consumers'); }
+  }, [shop]);
+
   return (
-    <Page headingText="Groupshop" onLogin={() => {}} onLogout={() => {}} onCreateAccount={() => {}}>
-      dashboard
-      {shop}
+    <>
+      <Head>
 
-      {installationDialogue()}
+        <meta name="facebook-domain-verification" content="e20mavbjsi9do1coqxjvn54hvcs69g" />
+      </Head>
+      <Page headingText="Groupshop" onLogin={() => { }} onLogout={() => { }} onCreateAccount={() => { }}>
+        dashboard
+        {shop}
 
-    </Page>
+        {installationDialogue()}
+
+      </Page>
+
+    </>
   );
 };
 
