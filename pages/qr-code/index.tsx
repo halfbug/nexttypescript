e67316b32e239e150c6234aabfc818b5082f57df
Amit Batra <86948970+amit-groupshop@users.noreply.github.com>
2022-11-02 13:52:56 +0530

    GS-1853 >> Update clarity code (#1385)
    Update clarity code

--- pages/qr-code/index.tsx
+++ pages/qr-code/index.tsx
@@ -28,7 +28,7 @@ const QrCode: NextPage = () => {
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "9j6d12pkzs");`,
+          })(window, document, "clarity", "script", "ecbk8j0pnn");`,
           }}
         />
       </Head>
