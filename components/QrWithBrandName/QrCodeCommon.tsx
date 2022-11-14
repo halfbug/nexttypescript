f69b9ac9addcb88e0699144c99c9d2d717d865c9
Komal Ata <komal.ata005@gmail.com>
2022-11-14 16:21:45 +0500

    GS-1922 (#1438)

--- components/QrWithBrandName/QrCodeCommon.tsx
+++ components/QrWithBrandName/QrCodeCommon.tsx
@@ -81,7 +81,7 @@ export default function QrCodeCommon() {
                     </Link>
                   </div>
                 )}
-              <div className={[styles.QRContainer__Logo, 'ms-5 ps-3 mt-5'].join(' ')}>
+              <div className={[styles.QRContainer__Logo, 'ms-4 mt-5'].join(' ')}>
                 { ShowAuth ? <HeadLogo /> : state.logoImage !== '' && (
                 <Link
                   href={{
