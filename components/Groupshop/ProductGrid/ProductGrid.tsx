2d4735a347248df00200c1b4ce15c84d429b57b2
Sadaf Siddiqui <92418104+sadafgs@users.noreply.github.com>
2023-03-31 21:35:05 +0500

    over 60 percent off from 65 (#1903)

--- components/Groupshop/ProductGrid/ProductGrid.tsx
+++ components/Groupshop/ProductGrid/ProductGrid.tsx
@@ -216,7 +216,7 @@ const ProductGrid = ({
                     <div className="d-flex align-items-center">
                       <div className={dStyles.drops_col_dropheading}>
                         {title === THE_VAULT_TITLE && 'The Vault: 30% - 50% Off'}
-                        {title === SPOTLIGHT_SECTION_TITLE && 'Spotlight: Over 65% Off '}
+                        {title === SPOTLIGHT_SECTION_TITLE && 'Spotlight: Over 60% Off '}
                       </div>
                       {/* <div className={dStyles.drops_col_dropheading_off}>
                         30% off
