552753d1be23b7321d529e2d69044a83a4a2cfa8
hardikbeladiya7 <92423900+hardikbeladiya7@users.noreply.github.com>
2023-04-05 18:36:33 +0530

    GS-2588 Fix share with friend label issue in pdp (#1911)

--- components/Groupshop/ProductDetail/ProductDetail.tsx
+++ components/Groupshop/ProductDetail/ProductDetail.tsx
@@ -848,7 +848,7 @@ const ProductDetail = ({
                       </>
                     ) : (
                       <ShareButton
-                        // label="share to unlock"
+                        label="share to unlock"
                         shareurl={isExpired ? shortActivateURL ?? activateURL : productShareUrl(product?.id ?? '')}
                         fullshareurl={isExpired ? activateURL : productShareUrl(product?.id ?? '')}
                         className={styles.groupshop_Pd_addtoCart}
