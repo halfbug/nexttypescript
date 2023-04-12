7b6eb53b1084244ea721979442794483364c815f
harishunk <82090890+harishunk@users.noreply.github.com>
2023-04-12 12:06:17 +0500

    GS-2628, UI for dynamic rewards with progress bar developed (#1931)

--- components/Groupshop/GradientProgressBar/GradientProgressBar.tsx
+++ components/Groupshop/GradientProgressBar/GradientProgressBar.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
 import styles from 'styles/GradientProgressBar.module.scss';
 
-interface IGradientProgress {
+interface IGradientProgressProps {
   progress: number,
-  className: string
+  className: string,
+  isRewardBased?: boolean,
+  rewards?: string[]
 }
 
-const GradientProgressBar = ({ progress, className }: IGradientProgress) => (
-  <div
-    style={{ backgroundSize: `${progress}%`, backgroundRepeat: 'repeat-y' }}
-    className={[styles.gradientProgressbar, className].join(' ')}
-  />
-);
+const GradientProgressBar = ({
+  progress, className, isRewardBased,
+  rewards = [],
+}: IGradientProgressProps) => {
+  const gradientBar = () => (
+    <div
+      style={{ backgroundSize: `${progress}%`, backgroundRepeat: 'repeat-y' }}
+      className={[styles.gradientProgressbar, className].join(' ')}
+    />
+  );
+  return (
+    <>
+      {!isRewardBased
+        ? (gradientBar())
+        : (
+          <div className="position-relative">
+            <div className={styles.gradientProgressbar__sectionsWrapper}>
+              {rewards.map((value, index) => (
+                <div
+                  className={index === 0
+                    ? styles.gradientProgressbar__section
+                    : styles.gradientProgressbar__sectionBorder}
+                />
+              ))}
+            </div>
+            {gradientBar()}
+            <div className={styles.gradientProgressbar__pillsWrapper}>
+              {rewards.map((value, index) => (
+                <div
+                  className={progress >= ((100 / rewards.length) * (index + 1))
+                    ? styles.gradientProgressbar__activePill
+                    : styles.gradientProgressbar__pill}
+                >
+                  {value}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+    </>
+  );
+};
+
+GradientProgressBar.defaultProps = {
+  isRewardBased: false,
+  rewards: [],
+};
 
 export default GradientProgressBar;
