259172b3b3ba37ba37a9533f3d3d94bd1d4e7d70
Jenish Pansuriya <93431910+jenishpansuriya@users.noreply.github.com>
2023-08-15 16:58:12 +0530

    GS-2929-drops some products are repeating few products being duplicated (#2115)
    Co-authored-by: mitesh patel <miteshpatel.7seasol@gmail.com>

--- hooks/useMergedVaultAndSpotlights.ts
+++ hooks/useMergedVaultAndSpotlights.ts
@@ -2,66 +2,71 @@ import { DROPS_ALLPRODUCT, DROPS_SPOTLIGHT, DROPS_VAULT } from 'configs/constant
 import { useCallback } from 'react';
 
 export default function useMergedVaultAndSpotlights() {
-  const completedFY: string[] = [];
-  const completed: string[] = [];
-
-  const findShopifyId = (data: any, Index: number) => {
-    if (Index !== -1) {
-      const shopifyId = data[Index]?.shopifyId;
-      return shopifyId;
-    }
-    return '';
-  };
-
-  const setAllProducts = (
-    data: any,
-    allProductElementIndex: number,
-    replaceElementIndex: number,
-    isForYou: boolean,
-    CshopifyID: string,
-    id: number,
-    sectionName?: string,
-  ) => {
-    const shopifyId = findShopifyId(data, replaceElementIndex);
-    const products = data?.filter((item: { shopifyId: string; }) => (isForYou
-      ? completedFY : completed).includes(item.shopifyId))
-      .map((item: { products: any; }) => item.products)?.flat();
-    return {
-      products: shopifyId ? [...products,
-        ...data[allProductElementIndex]?.products]
-        : products,
-      name: isForYou ? sectionName : 'All products',
-      shopifyId: CshopifyID,
-      type: isForYou ? 'forYou' : DROPS_ALLPRODUCT,
-      pageInfo: null,
-      id: isForYou ? id : '',
-    };
-  };
-
-  const getUpdatedShopifyID = (
-    csection: any,
-    vaultShopifyId: any,
-    spotlightShopifyId: any,
-    allProductShopifyId: any,
-    isForYou: boolean,
-  ) => {
-    let CshopifyID: any = csection?.shopifyId;
-
-    const idsToCheck = [vaultShopifyId, spotlightShopifyId, allProductShopifyId];
-    const completedIds = isForYou ? completedFY : completed;
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const id of idsToCheck) {
-      if (id && !completedIds.includes(id)) {
-        CshopifyID = id;
-        break;
-      }
-    }
-
-    return CshopifyID;
-  };
-
   const mergedVaultAndSpotlights = (dataForMearge: any, isForYou: boolean) => {
+    const completedFY: string[] = [];
+    const completed: string[] = [];
+
+    const findShopifyId = (data: any, Index: number) => {
+      if (Index !== -1) {
+        const shopifyId = data[Index]?.shopifyId;
+        return shopifyId;
+      }
+      return '';
+    };
+
+    const setAllProducts = (
+      data: any,
+      allProductElementIndex: number,
+      replaceElementIndex: number,
+      ForYou: boolean,
+      CshopifyID: string,
+      id: number,
+      sectionName?: string,
+    ) => {
+      let products = data?.filter((item: { shopifyId: string; }) => (ForYou
+        ? completedFY : completed).includes(item.shopifyId))
+        .map((item: { products: any; }) => item.products)?.flat();
+      if (!(isForYou
+        ? completedFY : completed).includes(data[replaceElementIndex].shopifyId)) {
+        products = [...products,
+          ...data[replaceElementIndex]?.products];
+      } else if (!(isForYou
+        ? completedFY : completed).includes(data[allProductElementIndex].shopifyId)) {
+        products = [...products,
+          ...data[allProductElementIndex]?.products];
+      }
+      return {
+        products,
+        name: isForYou ? sectionName : 'All products',
+        shopifyId: CshopifyID,
+        type: isForYou ? 'forYou' : DROPS_ALLPRODUCT,
+        pageInfo: null,
+        id: isForYou ? id : '',
+      };
+    };
+
+    const getUpdatedShopifyID = (
+      csection: any,
+      vaultShopifyId: any,
+      spotlightShopifyId: any,
+      allProductShopifyId: any,
+      ForYou: boolean,
+    ) => {
+      let CshopifyID: any = csection?.shopifyId;
+
+      const idsToCheck = [vaultShopifyId, spotlightShopifyId, allProductShopifyId];
+      const completedIds = ForYou ? completedFY : completed;
+
+      // eslint-disable-next-line no-restricted-syntax
+      for (const id of idsToCheck) {
+        if (id && !completedIds.includes(id)) {
+          CshopifyID = id;
+          break;
+        }
+      }
+
+      return CshopifyID;
+    };
     // Assuming your data is an array of elements
     const data = JSON.parse(JSON.stringify(isForYou ? dataForMearge?.sections : dataForMearge));
 
@@ -84,13 +89,15 @@ export default function useMergedVaultAndSpotlights() {
         const vaultShopifyId = findShopifyId(data, vaultElementIndex);
         const spotlightShopifyId = findShopifyId(data, spotlightElementIndex);
         const allProductShopifyId = findShopifyId(data, allProductElementIndex);
-        data.map((item: any) => {
-          if ((item?.pageInfo
-            && !item?.pageInfo?.hasNextPage
-            && item.products?.length <= 10) || item.products?.length < 10) {
-            (isForYou ? completedFY : completed).push(item.shopifyId);
+        const shopifyIds = [vaultShopifyId, spotlightShopifyId];
+
+        data.forEach((section:any) => {
+          if ((section?.pageInfo
+            && !section?.pageInfo?.hasNextPage) || section.products.length < 10) {
+            if (shopifyIds.includes(section.shopifyId)) {
+              (isForYou ? completedFY : completed).push(section.shopifyId);
+            }
           }
-          return item;
         });
 
         // find current section by shopifyId from data
@@ -136,7 +143,5 @@ export default function useMergedVaultAndSpotlights() {
   };
   return {
     mergedVaultAndSpotlights,
-    findShopifyId,
-    setAllProducts,
   };
 }
